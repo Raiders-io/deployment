fa8c2a0384ed605dd57f33fc6ce855ff58380bd5
Pierre Pontet <ppontet@student.42lyon.fr>
2026-06-15 10:58:30 +0200

    feat: added build stage to Dockerfile and removed volumes from compose.yml * chores: updated packages * chores: used linter to fix code style issues

--- srcs/api-gateway/src/index.ts
+++ srcs/api-gateway/src/index.ts
@@ -1,29 +1,37 @@
-import express from 'express';
-import type { Application } from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import http from 'http';
+import express from "express";
+import type { Application } from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import http from "http";
 
 const app: Application = express();
 const PORT: number = Number(process.env.PORT) || 3000;
-const MESSAGING_SERVICE_URL: string = process.env.MESSAGING_SERVICE_URL || 'http://messaging-service:3334';
-const AUTH_SERVICE_URL: string = process.env.AUTH_SERVICE_URL || 'http://auth-service:3333';
+const MESSAGING_SERVICE_URL: string =
+  process.env.MESSAGING_SERVICE_URL || "http://messaging-service:3334";
+const AUTH_SERVICE_URL: string =
+  process.env.AUTH_SERVICE_URL || "http://auth-service:3333";
 
 const messagingProxy = createProxyMiddleware({
-	target: MESSAGING_SERVICE_URL,
-	pathFilter: '/socket.io/**',
-	changeOrigin: true,
-	ws: true,
-})
+  target: MESSAGING_SERVICE_URL,
+  pathFilter: "/socket.io/**",
+  changeOrigin: true,
+  ws: true,
+});
 
-app.use('/test', (req, res) => {
+app.use("/test", (req, res) => {
   res.send("test route");
 });
 
-app.use(createProxyMiddleware({ target: AUTH_SERVICE_URL, pathFilter: '/auth/**', changeOrigin: true }));
-app.use(messagingProxy)
+app.use(
+  createProxyMiddleware({
+    target: AUTH_SERVICE_URL,
+    pathFilter: "/auth/**",
+    changeOrigin: true,
+  }),
+);
+app.use(messagingProxy);
 
-const server = http.createServer(app)
-server.on('upgrade', messagingProxy.upgrade)
+const server = http.createServer(app);
+server.on("upgrade", messagingProxy.upgrade);
 
 server.listen(PORT, () => {
   console.log(`Gateway running on port ${PORT}`);
