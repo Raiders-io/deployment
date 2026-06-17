1d7e4dc50180c0583dad3fd86ad6ca01f37206f9
Pierre Pontet <ppontet@student.42lyon.fr>
2026-06-17 11:51:04 +0200

    fix: certs for nginx generated too soon

--- setup_env.sh
+++ setup_env.sh
@@ -8,13 +8,24 @@ create_env()
 		read -p "Continue? (Y/n): " confirm
 		confirm=${confirm:-y} # Default to 'y' if no input is provided
 		if ! [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+			echo "Not replacing the file."
+			# echo "Setup cancelled."
+			# exit 1
+		else
+			cp .env.example .env
+		fi
+	fi
+
+	echo "Please replace the placeholder values in the .env file with your actual configuration."
+	if [ -f .env ]; then
+		echo "Did you finished configuring the .env file?"
+		read -p "Continue? (Y/n): " confirm
+		confirm=${confirm:-y} # Default to 'y' if no input is provided
+		if ! [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
 			echo "Setup cancelled."
 			exit 1
 		fi
 	fi
-
-	cp .env.example .env
-	echo "Please replace the placeholder values in the .env file with your actual configuration."
 	cd -
 }
 
