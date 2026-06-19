587322420631358e6eb51d82051aa67ea8cc8c9b
Pierre Pontet <ppontet@student.42lyon.fr>
2026-06-19 15:36:43 +0200

    fix: generate_certs prints error message on failure

--- srcs/nginx/generate_certs.sh
+++ srcs/nginx/generate_certs.sh
@@ -7,4 +7,9 @@ fi
 
 mkdir -p ./certs ./private
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./private/nginx-selfsigned.key -out ./certs/nginx-selfsigned.crt -subj "$OPENSSL_SUBJ" 2> tmp.error 1> tmp
+echo "Generating self-signed certificate for Nginx..."
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./private/nginx-selfsigned.key -out ./certs/nginx-selfsigned.crt -subj "$OPENSSL_SUBJ"
+if [ $? -ne 0 ]; then
+    echo "Error while generating self-signed certificate."
+    exit 1
+fi
